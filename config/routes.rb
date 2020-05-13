c27a9450788ede3f66cce81754fb5bd6c2c43b91
Ferruccio Sisti <ferrucciosisti@gmail.com>
2020-05-12 23:18:25 -0400

    added different title for scouting index (history)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/about'
   resources :scouters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'scouters#new'
+  get 'about', to: 'pages#about'
+  get 'history', to: 'scouters#index'
 end
